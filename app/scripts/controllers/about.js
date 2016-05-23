10aeb5bfdff84fead2ffbfb4fc2209360465ba7f
Leo Sandström <leosand97@gmail.com>
2016-05-23 17:17:49 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kattjaktxyzApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kattjaktxyzApp
+ */
+angular.module('kattjaktxyzApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kattjaktxyzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kattjaktxyzApp
+ */
+angular.module('kattjaktxyzApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
